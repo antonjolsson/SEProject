b7a8595916b648e9b8ea0d92978d848e7e607023
initGoomba <anton.j.olsson@bredband.net>
2019-09-24 19:15:13 +0200

    Created input controls

--- app/src/main/java/com/example/tripplannr/MainActivity.java
+++ app/src/main/java/com/example/tripplannr/MainActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
