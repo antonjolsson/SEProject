95c51a5c728052e177840686813f0293a68eac7f
initGoomba <anton.j.olsson@bredband.net>
2019-09-26 13:25:08 +0200

    Added (tentative) method for styling control icons

--- app/src/main/java/com/example/tripplannr/MainActivity.java
+++ app/src/main/java/com/example/tripplannr/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
