2764c4504d441d3bceb9ed077b31298878dc23c1
orgardj <orgardj@student.chalmers.se>
2019-09-26 16:05:38 +0200

    Revert "Merge branch 'orgardj/VasttrafikStopsData' into develop"
    This reverts commit 39a07ec71d9e36ffa60332f612dcb6da21f6bb61, reversing
    changes made to e553ef3c7b056f9fef8a1890d98beba4562fe0a0.

--- app/src/main/java/com/example/tripplannr/MapsActivity.java
+++ app/src/main/java/com/example/tripplannr/MapsActivity.java
@@ -4,7 +4,6 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-import com.example.tripplannr.stdanica.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
