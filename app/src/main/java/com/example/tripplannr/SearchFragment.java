a2f17e26e801cdb92186aa438a76d1a125c5bb10
initGoomba <anton.j.olsson@bredband.net>
2019-09-29 15:00:11 +0200

    Added location name formatting

--- app/src/main/java/com/example/tripplannr/SearchFragment.java
+++ app/src/main/java/com/example/tripplannr/SearchFragment.java
@@ -1,18 +1,13 @@
 package com.example.tripplannr;
 
 import android.os.Bundle;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import java.util.Objects;
@@ -20,33 +15,39 @@ import java.util.Objects;
 public class SearchFragment extends Fragment {
 
     private EditText toTextField, fromTextField;
+    private String name;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //toTextField = Objects.requireNonNull(getView()).findViewById(R.id.toText);
-        //fromTextField = Objects.requireNonNull(getView()).findViewById(R.id.fromText);
         TripViewModel model =
                 ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(TripViewModel.class);
         model.getOrigin().observe(this, new Observer<TripLocation>() {
             @Override
             public void onChanged(TripLocation tripLocation) {
-                fromTextField.setText(tripLocation.name);
+                name = formatLocationName(tripLocation.name);
+                fromTextField.setText(name);
             }
         });
         model.getDestination().observe(this, new Observer<TripLocation>() {
             @Override
             public void onChanged(TripLocation tripLocation) {
-                toTextField.setText(tripLocation.name);
+                name = formatLocationName(tripLocation.name);
+                toTextField.setText(name);
             }
         });
+    }
+
+    private String formatLocationName(String name) {
+        return name.replaceAll(",(\\s\\d+)+", ",").
+                replaceAll(",(\\s\\D+)+,(\\s\\D+)+", ",$1");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.search_frag, container, false);
         // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.search_frag, container, false);
+        View view = inflater.inflate(R.layout.search_frag, container, false);
+
         toTextField = Objects.requireNonNull(view).findViewById(R.id.toText);
         fromTextField = Objects.requireNonNull(view).findViewById(R.id.fromText);
         return view;
